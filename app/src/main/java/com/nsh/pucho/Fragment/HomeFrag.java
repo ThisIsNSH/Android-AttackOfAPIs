4c138dd0e9328b782e0a25c31e9f6445ffe8334b
ThisIsNSH <hadanis.singh@gmail.com>
2018-05-25 20:03:59 +0530

    dash

--- app/src/main/java/com/nsh/pucho/Fragment/HomeFrag.java
+++ app/src/main/java/com/nsh/pucho/Fragment/HomeFrag.java
@@ -46,7 +46,7 @@ public class HomeFrag extends Fragment implements View.OnClickListener {
     private List<Card> cardList = new ArrayList<>();
     private RecyclerView reco_rec, rece_rec;
     private CardAdapter mCardAdapter;
-    private TextView rect,acc_name, sign_out;
+    private TextView rect, acc_name, sign_out;
     private CircleImageView acc_img;
     private OnFragmentInteractionListener mListener;
     private RecyclerView recyclerView;
@@ -110,6 +110,36 @@ public class HomeFrag extends Fragment implements View.OnClickListener {
         reco_rec.setItemAnimator(new DefaultItemAnimator());
         reco_rec.setAdapter(mCardAdapter);
         prepareRecoData();
+        reco_rec.addOnItemTouchListener(new RecyclerTouchListener(getContext(), reco_rec, new RecyclerTouchListener.ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+
+                Dialog dialog = new Dialog(getContext());
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setContentView(R.layout.card_aws2);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+                dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                dialog.show();
+                String one = "Google APIs:\nA request is sent with ApiKey query with json Reuqest which fetches a Unique Id. This id therfore acts as query in fetching json response.\n\nAmazon APIs:\nJson request is sent with specific headers including credentials which fetch us the response.\n\nIBM Watson API:\nThe Chatbot is built using it. Assistant has its function which take request and pass on to the url which fetch us the response processed in our online workspace.";
+                String two = "Firebase:\nLogin and credentials are handled using firebase. The unified login platform is provided which allows better tracking of the users.\n\nSQL:\nThe database to handle recent activity is done using SQL. The database is created within the mobile to increase productivity.";
+                String three = "Circular Image View\nAndroid Design/Support\nAmazon SDK\nPicasso\nWave View for Login\nFlow Layout Manager\nFirebase SDK";
+                String four = "To provide unified theme to the app card views are linked with recycler view. \nDue to better design attributes of card view it is preffered. Proper padding and margins has overcomed the corner overlay problem.";
+
+                ArrayList<String> m = new ArrayList<String>();
+                m.add(one);
+                m.add(two);
+                m.add(three);
+                m.add(four);
+                TextView a = dialog.findViewById(R.id.text123);
+                a.setText(m.get(position));
+
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));
 
         recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getContext(), recyclerView, new RecyclerTouchListener.ClickListener() {
             @Override
@@ -164,7 +194,7 @@ public class HomeFrag extends Fragment implements View.OnClickListener {
     }
 
     private void prepareRecoData() {
-        Card card = new Card("APIs", "https://inform.tmforum.org/wp-content/uploads/2014/08/APIs-shutterstock_210232156-2-702x336.jpg", "Code + Implementation");
+        Card card = new Card("APIs", "https://inform.tmforum.org/wp-content/uploads/2014/08/APIs-shutterstock_210232156-2-702x336.jpg", "Implementation");
         cardList.add(card);
 
 
